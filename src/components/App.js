b79602d30f0e14112ad3c33f964bb96f00060c2c
revanzli <revanzekeryeli2002@gmail.com>
2022-05-09 13:08:28 +0400

    feat: added home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import PrivateRouter from './common/PrivateRouter'
+import Home from './containers/home/Home'
 
 const App = () => {
     return (
         <BrowserRouter>
             <Route element={<PrivateRouter />}>
+                <Route element={<Home />} path={'/'} />
             </Route>
-            <div>Chat App</div>
         </BrowserRouter>
     )
 }
