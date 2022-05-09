62079d649a3a2db9e75a11f179374ca17c9b2b47
revanzli <revanzekeryeli2002@gmail.com>
2022-05-09 13:21:13 +0400

    feat: server connection start

--- src/components/common/PrivateRouter.js
+++ src/components/common/PrivateRouter.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr'
+import React, { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 
 const PrivateRouter = () => {
+    const [connection, setConnection] = useState({})
+    const server = 'https://localhost:5001/chathub'
+    useEffect(() => {
+        const newConnection = new HubConnectionBuilder()
+            .withUrl(server)
+            .configureLogging(LogLevel.Information)
+            .build();
+        newConnection.start();
+        setConnection(newConnection);
+    }, []);
+
     return <Outlet />
 }
 
