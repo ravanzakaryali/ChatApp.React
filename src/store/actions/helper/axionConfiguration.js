04cbdd7b12f5d4682584f08e3f238224e43f5386
revanzli <revanzekeryeli2002@gmail.com>
2022-05-15 05:43:57 +0400

    feat: added axios configuration

---
+++ src/store/actions/helper/axionConfiguration.js
@@ -0,0 +1,7 @@
+export const baseUrl = "https://localhost:5001/api";
+export const header = {
+    header: {
+        'Accept': 'application/json, text/plain',
+        'Content-Type': 'application/json;charset=UTF-8'
+    },
+}
