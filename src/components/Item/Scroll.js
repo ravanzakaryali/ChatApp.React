d53b3fa7e2bc302533818688c0d4fb956a15c69a
revanzli <revanzekeryeli2002@gmail.com>
2022-05-24 18:23:33 +0400

    refactor: modified scroll

--- src/components/Item/Scroll.js
+++ src/components/Item/Scroll.js
@@ -1,7 +1,8 @@
 import styled from '@emotion/styled';
 
 const Scroll = styled('div')(({ theme }) => ({
-    overflowY: "scroll"
+    overflowY: "scroll",
+    height: `calc(100vh - 250px)`,
 }))
 
 export default Scroll;
