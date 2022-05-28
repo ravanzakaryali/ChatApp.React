7117c3258527b9782e66bd91271ac953d4c42179
revanzli <revanzekeryeli2002@gmail.com>
2022-05-28 10:16:42 +0400

    feat: added conversation message

---
+++ src/components/Chats/ConversationMessage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ConversationMessage = () => {
+    return (
+        <div>
+            <div className='chat-avatar'>
+            </div>
+            <div className='chat-parent'>
+                <div className='row'>
+                    <div className='chat-content'>
+                    </div>
+                    <div className='chat-name'>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ConversationMessage
