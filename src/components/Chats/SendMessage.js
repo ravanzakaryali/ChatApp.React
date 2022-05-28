0dba26f75d2370e3aa623f538edcd61286acca03
revanzli <revanzekeryeli2002@gmail.com>
2022-05-28 18:20:52 +0400

    refactor: added icons and button

--- src/components/Chats/SendMessage.js
+++ src/components/Chats/SendMessage.js
@@ -2,26 +2,44 @@ import { TextFields } from '@mui/icons-material'
 import { InputBase } from '@mui/material'
 import React from 'react'
 import Row from '../Item/Row'
+import SendMessageIcons from './SendMessageIcons'
 
 const SendMessage = () => {
     return (
         <Row sx={{
             padding: "5px 0px"
         }}>
-            <InputBase
-                sx={{
-                    ml: 1,
-                    flex: 1,
-                    border: 'solid 1px gray',
-                    padding: "8px 15px",
-                    borderRadius: "10px",
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                console.log(e.target.sendmessage.value);
+                e.target.reset();
+            }}
+                style={{
+                    display: 'flex',
+                    width: '100%'
                 }}
-                inputProps={{ 'placeholder': 'Enter your message....' }}
-            />
-            {/* <SendMessageIcons /> */}
-            {/* <SendButton/> */}
+            >
+                <InputBase
+                    name='sendmessage'
+                    fullWidth
+                    sx={{
+                        width: '100%',
+                        ml: 1,
+                        flex: 1,
+                        border: 'solid 1px gray',
+                        padding: "8px 15px",
+                        borderRadius: "10px",
+                    }}
+                    inputProps={{ 'placeholder': 'Enter your message....' }}
+                />
+                <SendMessageIcons />
+                <button type='submit'>
+                    Send
+                </button>
+                {/* <SendButton/> */}
+            </form>
         </Row>
     )
 }
 
-export default SendMessage
+export default SendMessage;
--- src/components/Chats/SendMessageIcons.js
+++ src/components/Chats/SendMessageIcons.js
@@ -1,10 +1,23 @@
+import { ImportContacts } from '@mui/icons-material'
 import React from 'react'
+import ChatNavButton from '../Item/ChatNavButton'
 import Row from '../Item/Row'
 
 const SendMessageIcons = () => {
     return (
         <Row>
-
+            <ChatNavButton>
+                <ImportContacts />
+            </ChatNavButton>
+            <ChatNavButton>
+                <ImportContacts />
+            </ChatNavButton>
+            <ChatNavButton>
+                <ImportContacts />
+            </ChatNavButton>
+            <ChatNavButton>
+                <ImportContacts />
+            </ChatNavButton>
         </Row>
     )
 }
