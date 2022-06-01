e4b3decb63dcd22c02998feb3954990c2c0c2886
revanzli <revanzekeryeli2002@gmail.com>
2022-06-01 20:37:59 +0400

    refactor: modified use form login

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,11 +1,15 @@
 import { Button, FormControl, TextField, Typography } from '@mui/material'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import Form from '../../components/Item/Form'
 
 const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
             <Typography
                 sx={{
                     marginBottom: '0.5rem',
@@ -15,27 +19,24 @@ const Login = () => {
             </Typography>
             <FormControl fullWidth>
                 <TextField
-                    sx={{
-                        marginBottom: '1rem',
-                    }}
-                    id="username" label="Username" variant="outlined" />
+                    {...register("userName", { required: true })}
+                    sx={{ marginBottom: '0.2rem' }} id="userName" label="Username" variant="outlined" />
+                {errors.userName && <Typography color="red">Username is required</Typography>}
+
                 <TextField
-                    sx={{
-                        marginBottom: '1rem',
-                    }}
-                    id="password" label="Password" variant="outlined" />
-                <FormControl
-                    sx={{
-                        marginBottom: '1rem',
-                    }}
-                >
-                    <Link
-                        to='/auth/register'
-                    >New account</Link>
+                    type="password"
+                    {...register("password", { required: true, })}
+                    sx={{ margin: '1rem 0 0.2rem 0' }} id="password" label="Password" variant="outlined" />
+                {errors.password && <Typography color="red">Password is required</Typography>}
+
+                <FormControl sx={{ margin: '0.5rem 0' }}>
+                    <Link to='/auth/register'> New account </Link>
                 </FormControl>
+
                 <Button type='submit' variant='contained'>
                     Login
                 </Button>
+
             </FormControl>
         </Form>
 
